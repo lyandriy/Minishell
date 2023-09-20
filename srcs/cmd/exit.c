4c9c6339501523783612282e4fc46f05a820d9e1
Vicente Garcia Andrade <vicgarci@c2r7s6.42madrid.com>
2023-09-20 16:55:00 +0200

    EXIT arreglado caso a 1

--- srcs/cmd/exit.c
+++ srcs/cmd/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lyandriy <lyandriy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 18:52:01 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/08/19 17:12:04 by lyandriy         ###   ########.fr       */
+/*   Updated: 2023/09/20 16:54:17 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,6 @@ void	ft_exit(t_shell *shell, t_cmd *cmd)
 	int		size;
 
 	exit_value = 0;
-	size = get_argv_size(cmd->argv);
-	if (size == FT_INVALID_POS)
-	{
-		ft_printf("Minishell: exit: too many arguments\n");
-		g_shell->child_status = 1;
-		return ;
-	}
 	if (cmd->argv[1])
 	{
 		if (!is_all_number(cmd->argv[1]))
@@ -42,12 +35,19 @@ void	ft_exit(t_shell *shell, t_cmd *cmd)
 			ft_printf("bash: %s: numeric argument required\n",
 				cmd->argv[1]);
 			exit_value = 255;
+			exit(exit_value);
 		}
 		else
 			with_exit_value(&exit_value, cmd);
 	}
-	ft_cleanshell(shell);
-	exit(exit_value);
+	size = get_argv_size(cmd->argv);
+	if (size > 2)
+		ft_printf("Minishell: exit: too many arguments\n");
+	if (size == 1)
+	{
+		ft_cleanshell(shell);
+		exit(exit_value);
+	}
 }
 
 static int	get_argv_size(char **argv)
@@ -57,8 +57,7 @@ static int	get_argv_size(char **argv)
 	i = 0;
 	while (argv[i])
 		i++;
-	if (i > 2)
-		return (FT_INVALID_POS);
+
 	return (i);
 }
 
